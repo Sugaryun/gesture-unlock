d091434ae2c3a2fe4cced3796c01b8e9265d08ee
hechangyuan <878917909@qq.com>
2020-10-12 17:43:49 +0800

    仿谷歌首页

---
+++ demo1.js
@@ -0,0 +1,32 @@
+var arrLists = [
+    {
+        id: '001',
+        title: "腾讯企业邮箱",
+        img: "https://pic4.zhimg.com/v2-0a9237b73f78d2fd44543304a1f4aabe_xs.jpg?source=1940ef5c"
+    },
+    {
+        id: '002',
+        title: "腾讯企业邮箱",
+        img: "https://pic4.zhimg.com/v2-0a9237b73f78d2fd44543304a1f4aabe_xs.jpg?source=1940ef5c"
+    },
+    {
+        id: '003',
+        title: "腾讯企业邮箱",
+        img: "https://pic4.zhimg.com/v2-0a9237b73f78d2fd44543304a1f4aabe_xs.jpg?source=1940ef5c"
+    },
+    {
+        id: '004',
+        title: "腾讯企业邮箱",
+        img: "https://pic4.zhimg.com/v2-0a9237b73f78d2fd44543304a1f4aabe_xs.jpg?source=1940ef5c"
+    },
+    {
+        id: '005',
+        title: "腾讯企业邮箱",
+        img: "https://pic4.zhimg.com/v2-0a9237b73f78d2fd44543304a1f4aabe_xs.jpg?source=1940ef5c"
+    },
+    {
+        id: '006',
+        title: "腾讯企业邮箱",
+        img: "https://pic4.zhimg.com/v2-0a9237b73f78d2fd44543304a1f4aabe_xs.jpg?source=1940ef5c"
+    }
+]
